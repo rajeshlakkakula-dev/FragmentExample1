6c9f8c19ac651e11bddfb5f5537c75ae7758e983
Rajesh Lakkakula <rajeshlakkakula536@gmail.com>
2022-08-26 13:18:18 +0530

    This commit contains "Fragment Example 1"
    Add Fragments to Activity

---
+++ app/src/main/java/com/rl/fragmentexample1/MainActivity.java
@@ -0,0 +1,75 @@
+package com.rl.fragmentexample1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button button;
+    boolean isFragmentDisplayed = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!isFragmentDisplayed){
+                    displayFragment();
+                }else{
+                    closeFragment();
+                }
+            }
+        });
+
+
+
+    }
+
+    public void displayFragment() {
+
+        SimpleFragment simpleFragment = SimpleFragment.newInstance();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.add(R.id.fragment_container, simpleFragment).addToBackStack(null).commit();
+
+        button.setText(R.string.close);
+
+        isFragmentDisplayed = true;
+
+    }
+
+
+    public void closeFragment() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        SimpleFragment simpleFragment = (SimpleFragment) fragmentManager.findFragmentById(R.id.fragment_container);
+
+        if (simpleFragment != null) {
+
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+            fragmentTransaction.remove(simpleFragment).commit();
+        }
+        button.setText(R.string.open);
+        isFragmentDisplayed = false;
+    }
+
+
+
+
+
+}
